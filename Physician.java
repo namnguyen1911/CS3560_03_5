1fac3497af742ce4fc9c572ec864a9aebdc1ab8e
Nam Nguyen <namnguyen@cpp.edu>
2021-10-08 21:26:48 -0700

    Implement Physician.java

--- Physician.java
+++ Physician.java
@@ -1,3 +1,22 @@
-public class Physician {
-    
+public class Physician extends Person{
+    private int physician_ID;
+
+    public Physician() {
+        super();
+        physician_ID = 0;
+    }
+
+    public Physician(int newID) {
+        super();
+        physician_ID = newID;
+    }
+
+    public void setPhysicianID(int newID) {
+        physician_ID = newID;
+    }
+
+    public int getPhysicianID() {
+        return physician_ID;
+    }
+
 }
