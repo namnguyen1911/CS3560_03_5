6c2b3d8a019c007528f25d37dca03ce351f3d854
Nam Nguyen <namnguyen@cpp.edu>
2021-10-08 19:32:03 -0700

    Add files

---
+++ Appointment.java
@@ -0,0 +1,36 @@
+public class Appointment {
+    private int physician_ID;
+    private int patient_ID;
+    private int availability_ID;
+    private String reason;
+
+    public Appointment() {
+        physician_ID = 0;
+        patient_ID = 0;
+        availability_ID = 0;
+        reason = "";
+    }
+
+    public Appointment(int newPhysicianID, int newPatientID, int newTime, String newReason) {
+        physician_ID = newPhysicianID;
+        patient_ID = newPatientID;
+        availability_ID = newTime;
+        reason = newReason;
+    }
+
+    public int getPhysicianID() {
+        return physician_ID;
+    }
+
+    public int getPatientID( ) {
+        return patient_ID;
+    }
+
+    public void setReason(String newReason) {
+        reason = newReason;
+    }
+
+    public int getAvailability() {
+        return availability_ID;
+    }
+}
---
+++ patient_Scheduling.java
@@ -0,0 +1,3 @@
+public class patient_Scheduling {
+    
+}
