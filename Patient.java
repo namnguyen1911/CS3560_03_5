020dfa622b2b3d286b18a9e089ecf851873fcb15
Nam Nguyen <namnguyen@cpp.edu>
2021-10-08 20:35:15 -0700

    Implement Patient.java

--- Patient.java
+++ Patient.java
@@ -1,3 +1,20 @@
 public class Patient {
-    
+    private int patient_ID;
+
+    public Patient() {
+        super();
+        patient_ID = 0;
+    }
+
+    public Patient(int newID) {
+        patient_ID = newID;
+    }
+
+    public void setPatientID(int newID) {
+        patient_ID = newID;
+    }
+
+    public int getPatientID() {
+        return patient_ID;
+    }
 }
